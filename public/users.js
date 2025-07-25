93c18d5c8ca7fa41dc439bdfffaa5f42cb146b53
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-25 21:57:54 +0300

    Create users.js

---
+++ public/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    username: "demo1",
+    password: "demo123",
+    plan: "daily", // daily, weekly, monthly, etc.
+    signalsToday: 1
+  },
+  {
+    username: "demo2",
+    password: "demo456",
+    plan: "yearly",
+    signalsToday: 3
+  }
+];
