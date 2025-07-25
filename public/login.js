6ab9c465ecdd39679688a4f16f57e20e08b8190c
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-25 22:10:23 +0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,12 +1,13 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-  if (username === "demo" && password === "1234") {
-    // Güvenli yönlendirme
-    window.location.href = "./panel.html";
-  } else {
-    alert("Invalid credentials. Please try again.");
-  }
+    if (username === "demo" && password === "1234") {
+      window.location.href = "panel.html"; // Yönlendirme
+    } else {
+      alert("Invalid credentials. Please try again.");
+    }
+  });
 });
