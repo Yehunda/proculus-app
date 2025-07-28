879f789b7177e061659e1b4c995f6898bcb51ac3
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-28 08:13:24 +0300

    Update and rename web3modal/wagmi.js to wagmi.js

---
+++ wagmi.js
@@ -0,0 +1,15 @@
+// wagmi.js
+export {
+  configureChains,
+  createConfig
+} from 'https://unpkg.com/@wagmi/core@1.4.0/dist/index.js';
+
+export {
+  mainnet,
+  polygon,
+  avalanche,
+  arbitrum,
+  optimism,
+  base,
+  bsc
+} from 'https://unpkg.com/@wagmi/chains@1.4.0/dist/index.js';
