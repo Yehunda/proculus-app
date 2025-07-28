c5e50f4ac838b5d5d070075130ca66b1d4e0bd39
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-28 08:51:07 +0300

    Update ethereum.js

--- ethereum.js
+++ ethereum.js
@@ -1,35 +1,3 @@
-export const EVM_CHAINS = {
-  1: {
-    chainId: "0x1",
-    chainName: "Ethereum Mainnet",
-    nativeCurrency: {
-      name: "Ether",
-      symbol: "ETH",
-      decimals: 18,
-    },
-    rpcUrls: ["https://rpc.ankr.com/eth"],
-    blockExplorerUrls: ["https://etherscan.io"],
-  },
-  137: {
-    chainId: "0x89",
-    chainName: "Polygon",
-    nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
-      decimals: 18,
-    },
-    rpcUrls: ["https://polygon-rpc.com"],
-    blockExplorerUrls: ["https://polygonscan.com"],
-  },
-  42161: {
-    chainId: "0xa4b1",
-    chainName: "Arbitrum One",
-    nativeCurrency: {
-      name: "Ether",
-      symbol: "ETH",
-      decimals: 18,
-    },
-    rpcUrls: ["https://arb1.arbitrum.io/rpc"],
-    blockExplorerUrls: ["https://arbiscan.io"],
-  }
-};
+// ethereum.js
+
+export { EthereumClient, w3mConnectors, w3mProvider } from 'https://unpkg.com/@web3modal/ethereum@2.7.2/dist/index.js';
