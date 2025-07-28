44f1f5f64b74932f092ed01686f8d5ce533ba7ed
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-28 18:19:09 +0300

    Update walletconnect.js

--- walletconnect.js
+++ walletconnect.js
@@ -2,7 +2,7 @@ import { Web3Modal } from './web3modal/html.js';
 import { EthereumClient, w3mConnectors, w3mProvider } from './web3modal/ethereum.js';
 import { configureChains, createConfig } from './web3modal/core.js';
 import { mainnet, polygon, avalanche, arbitrum, optimism, base, bsc } from './web3modal/chains.js';
-const projectId = 'demo'; // kendi Project ID’n varsa buraya yaz
+const projectId = 'a64004a6d07f7e8f7c8a2bb7b8c0fa50'; 
 
 const chains = [mainnet, polygon, avalanche, arbitrum, optimism, base, bsc];
 
