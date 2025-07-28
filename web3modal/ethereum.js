ea0588f346980e73670a3e6e911b89f9a7f7a327
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-28 03:40:45 +0300

    Update ethereum.js

--- web3modal/ethereum.js
+++ web3modal/ethereum.js
@@ -1,2 +1,4 @@
-// Local placeholder for ethereum.js
-console.log('ethereum.js loaded');
+import { EthereumClient } from '@web3modal/ethereum';
+import { w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+
+export { EthereumClient, w3mConnectors, w3mProvider };
