7ad1bbb90303c6b15d7171a5cc54212c510a4f03
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-28 08:52:05 +0300

    Update wagmi.js

--- wagmi.js
+++ wagmi.js
@@ -1,21 +1,115 @@
-import { configureChains, createConfig } from '@wagmi/core';
-import { publicProvider } from '@wagmi/core/providers/public';
-import {
-  mainnet,
-  polygon,
-  arbitrum,
-  avalanche,
-  optimism,
-  base,
-  bsc,
-} from '@wagmi/core/chains';
+// wagmi.js
 
-export const { chains, publicClient } = configureChains(
-  [mainnet, polygon, arbitrum, avalanche, optimism, base, bsc],
-  [publicProvider()],
-);
+export { configureChains, createConfig } from 'https://unpkg.com/@wagmi/core@1.4.0/dist/index.js';
 
-export const wagmiConfig = createConfig({
-  autoConnect: true,
-  publicClient,
-});
+export const mainnet = {
+  id: 1,
+  name: 'Ethereum',
+  network: 'mainnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.ankr.com/eth']
+    }
+  }
+};
+
+export const polygon = {
+  id: 137,
+  name: 'Polygon',
+  network: 'polygon',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'MATIC',
+    symbol: 'MATIC',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://polygon-rpc.com']
+    }
+  }
+};
+
+export const avalanche = {
+  id: 43114,
+  name: 'Avalanche',
+  network: 'avalanche',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'AVAX',
+    symbol: 'AVAX',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://api.avax.network/ext/bc/C/rpc']
+    }
+  }
+};
+
+export const arbitrum = {
+  id: 42161,
+  name: 'Arbitrum',
+  network: 'arbitrum',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://arb1.arbitrum.io/rpc']
+    }
+  }
+};
+
+export const optimism = {
+  id: 10,
+  name: 'Optimism',
+  network: 'optimism',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://mainnet.optimism.io']
+    }
+  }
+};
+
+export const base = {
+  id: 8453,
+  name: 'Base',
+  network: 'base',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://mainnet.base.org']
+    }
+  }
+};
+
+export const bsc = {
+  id: 56,
+  name: 'Binance Smart Chain',
+  network: 'bsc',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'BNB',
+    symbol: 'BNB',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://bsc-dataseed.binance.org/']
+    }
+  }
+};
