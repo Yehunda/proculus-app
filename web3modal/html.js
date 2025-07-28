523b10324b63d90eeb958d5285b12b5ad7673e83
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-28 05:10:51 +0300

    Update html.js

--- web3modal/html.js
+++ web3modal/html.js
@@ -1,2 +1,13 @@
 // html.js
-export { Web3Modal } from 'https://unpkg.com/@web3modal/html@2.6.3/dist/index.js';
+
+export class Web3Modal {
+  constructor(options, ethereumClient) {
+    this.options = options;
+    this.ethereumClient = ethereumClient;
+  }
+
+  openModal() {
+    const walletUrl = 'https://walletconnect.com/';
+    window.open(walletUrl, '_blank', 'width=600,height=700');
+  }
+}
