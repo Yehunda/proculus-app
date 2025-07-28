e41024f4ec36a73043c5b72a6e72549dfdfd7682
Yehunda <122574861+Yehunda@users.noreply.github.com>
2025-07-28 08:10:16 +0300

    Update and rename web3modal/ethereum.js to ethereum.js

---
+++ ethereum.js
@@ -0,0 +1,2 @@
+// ethereum.js
+export { EthereumClient, w3mConnectors, w3mProvider } from 'https://unpkg.com/@web3modal/ethereum@2.7.2/dist/index.js';
--- web3modal/ethereum.js
+++
@@ -1,3 +0,0 @@
-// ethereum.js
-import { EthereumClient, w3mConnectors, w3mProvider } from 'https://unpkg.com/@web3modal/ethereum@2.6.3/dist/index.js';
-export { EthereumClient, w3mConnectors, w3mProvider };
